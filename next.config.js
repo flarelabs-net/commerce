39b373b7494b9c16500a594142e84f32af23f514
Victor Berchet <victor@suumit.com>
2024-09-13 17:57:17 +0200

    updates for the builder

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  experimental: {
+    serverMinification: false
+  },
+  compress: false,
+
   images: {
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
